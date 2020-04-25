865c9205d0d0957df3c9749f59dd025174fa858b
Richeve S. Bebebdor <richeve.bebedor@gmail.com>
2020-04-26 01:37:46 +0800

    Added basic test.

--- callback-once.test.js
+++ callback-once.test.js
@@ -4,8 +4,42 @@ const assert = require( "assert" );
 
 const strictAssert = assert.strict;
 
-(
-	async	function( ){
+const callbackOnce = require( "./callback-once.js" );
 
-			}
+(
+	function( ){
+		const testCallback = (
+			callbackOnce(
+				function testCallback( ){
+					return	true;
+				}
+			)
+		);
+
+		strictAssert
+		.equal(
+			(
+				testCallback( )
+			),
+			(
+				true
+			),
+			(
+				"Test callback procedure must return true."
+			)
+		);
+
+		strictAssert
+		.throws(
+			(
+				testCallback
+			),
+			(
+				/\#callback\-called\-more\-than\-once\;/
+			),
+			(
+				"Test callback must throw Error#callback-called-more-than-once;"
+			)
+		);
+	}
 )( );
